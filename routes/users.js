044e8d9117cb0d49c375fcc00df5a0c2fcf3d1e3
VIKRAM SINGH <145076898+vikramsingh9219251196@users.noreply.github.com>
2024-02-14 18:06:23 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ import express from "express";
 import { login, signup } from "../controllers/auth.js";
 import { getAllUsers, updateProfile } from "../controllers/users.js";
 import auth from "../middleware/auth.js";
-// import { generateOTP, verifyOTP } from "../utility/otpGenerator.js";
 
 const router = express.Router();
 
@@ -11,7 +10,5 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.get("/getAllUsers", getAllUsers);
 router.patch("/update/:id", auth, updateProfile);
-// router.post("/generate-otp", generateOTP);
-// router.post("/verify-otp", verifyOTP);
 
 export default router;
