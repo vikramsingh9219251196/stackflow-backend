eff01f8f9dc0c5a62ce028b5025cde5fb3b92eb1
VIKRAM SINGH <145076898+vikramsingh9219251196@users.noreply.github.com>
2024-02-14 15:36:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,19 +13,7 @@ connectDB();
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-const allowedOrigins = ['https://stackflow-frontend.vercel.app/'];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
-
-app.use(cors(corsOptions)); 
+app.use(cors());
 app.use("/user", userRoutes);
 app.use("/questions", questionRoutes);
 app.use("/answer", answerRoutes);
